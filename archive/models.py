2e2f19ec389e031c72f44fbbebc009dc7ddd74fe
faagerholm <fagerholm.jimmy@gmail.com>
2018-09-07 16:45:07 +0300

    implementing models

--- archive/models.py
+++ archive/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class File(models.Model):
+    title = models.CharField(max_length=150)
+    pub_date = models.DateTimeField()
+    file = models.FileField()
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+
+class Picture(File):
+    file = models.ImageField(upload_to='/Images')
