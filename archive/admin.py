2a1ce576d0d1dcd5e5cc062efdc8ee6f39feed59
faagerholm <fagerholm.jimmy@gmail.com>
2018-09-09 11:22:35 +0300

    Cleaned up wrong commit, now work on cleaning up admin panel.

--- archive/admin.py
+++ archive/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.utils.text import format_lazy
 
-# Register your models here.
+from .models import Collection, AbstractFile
+
+
+def get_file_preview(obj):
+    if obj.pk:
+        return """<a href="{src}" target="_blank"><img src="{src}" alt="{title}" style="max-width: 200px; max-height: 200px;" /></a>""".format(
+                src=obj.BaseFile.url,
+                title=obj.title,
+        )
+    return _("(choose a file and save and continue editing to see the preview)")
+
+
+get_file_preview.allow_tags = True
+get_file_preview.short_description = _("File Preview")
+
+
+class FileInline(admin.StackedInline):
+    model = AbstractFile
+    extra = 0
+    fields = ["get_edit_link", "title", "file", get_file_preview]
+    readonly_fields = ["get_edit_link", get_file_preview]
+
+    def get_edit_link(self, obj=None):
+        if obj.pk:  # if object has already been saved and has a primary key, show link to it
+            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[format_lazy(obj.pk)])
+            return """<a href="{url}">{text}</a>""".format(
+                url=url,
+                text=_("Edit this %s separately") % obj._meta.verbose_name,
+            )
+        return _("(save and continue editing to create a link)")
+    get_edit_link.short_description = _("Edit link")
+    get_edit_link.allow_tags = True
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    save_on_top = True
+    fields = ['title']
+    inlines = [FileInline]
+
+
+@admin.register(AbstractFile)
+class BaseFileAdmin(admin.ModelAdmin):
+    save_on_top = True
+    fields = ['belongs_in', 'title', 'baseFile', get_file_preview]
+    readonly_fields = [get_file_preview]
--- archive/models.py
+++ archive/models.py
@@ -1,33 +1,48 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
+import datetime
+import os
 
-# Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(_('title'), max_length=100)
+    pub_date = models.DateTimeField(default=datetime.datetime.now())
+
+    class Meta:
+        verbose_name =_('Collection')
+        verbose_name_plural =_('Collections')
+
+    def __str__(self):
+        return self.title
+
+
+def upload_to(instance, filename):
+    filename_base, filename_ext = os.path.splitext(filename)
+    return "collection/{collection}/{filename}{extension}".format(
+        collection=slugify(instance.belongs_in.title),
+        filename=slugify(filename_base),
+        extension=filename_ext.lower(),
+    )
 
 
 class AbstractFile(models.Model):
-    title = models.CharField(max_length=150)
-    pub_date = models.DateTimeField()
+    """
+        abstract class for all fileTypes, defines all common characters.
+    """
+
+    belongs_in = models.ForeignKey(Collection, verbose_name=_('Collection'), on_delete=models.CASCADE)
+    title = models.CharField(_('Namn'), max_length=100, blank=True)
+    pub_date = models.DateTimeField(default=datetime.datetime.now())
+    file = models.FileField(_('Fil'), blank=True, upload_to=upload_to)
 
     class Meta:
-        abstract = True
+        verbose_name = _("fil")
+        verbose_name_plural = _("filer")
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
 
     def __str__(self):
         return self.title
-
-
-class File(AbstractFile):
-
-    class Meta:
-        verbose_name = _('fil')
-        verbose_name_plural = _('filer')
-
-
-class Picture(AbstractFile):
-    file = models.ImageField(upload_to='uploads/images')
-
-    class Meta:
-        verbose_name = _('bild')
-        verbose_name_plural = _('bilder')
