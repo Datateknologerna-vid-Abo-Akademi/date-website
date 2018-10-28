af2dad42ea21b708ffad9bdff8bb00d588f48734
siikasmaa <j.otto.siikasmaa@gmail.com>
2018-10-28 11:36:45 +0200

    Added first test

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from events.models import Event
+from members.models import Member
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        member = Member.objects.create(username='Test')
+        Event.objects.create(title='Test event', slug='test', author_id=member.id)
+
+    def test_attending_event(self):
+        event = Event.objects.get(slug='test')
+        self.assertIsNotNone(event)
+        c = Client()
+        response = c.post(reverse('events:detail', args=[event.slug]), {'user': 'person', 'email': 'person@test.com'})
+        self.assertEqual(response.status_code, 200)
