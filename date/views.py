fcb1fb1d85c6e5017b602cb98e81f9476f911f00
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2020-02-17 22:12:38 +0200

    added 404/500 error views

--- date/views.py
+++ date/views.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.shortcuts import render, redirect
+from django.template import RequestContext
 from django.utils import translation
 
 from django.conf import settings
@@ -29,3 +30,15 @@ def language(request, lang):
     # response.set_cookie()
     origin = request.META.get('HTTP_REFERER')
     return redirect(origin)
+
+
+def handler404(request, *args, **argv):
+    response = render(request, '404.html', {})
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render(request, '500.html', {})
+    response.status_code = 404
+    return response
