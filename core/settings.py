3f0325c44cc8455f0de94ab0aa05e09ad31fc1a9
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2020-02-24 13:45:32 +0200

    Removed settings env email_url

--- core/settings.py
+++ core/settings.py
@@ -208,7 +208,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During develo
 EMAIL_USE_TLS = True
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env.email_url('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = env('EMAIL_PORT')
 
 LOGGING = {
