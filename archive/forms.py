503a36af3499be24997a2828bd5f5716104ea4a9
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2020-03-13 09:51:06 +0200

    #26: Upload multiple files at once in Admin panel. Works well for editing a collection as well.

--- archive/forms.py
+++ archive/forms.py
@@ -1,6 +1,36 @@
 from django import forms
+from .models import Collection, Picture
 
 
 class PictureUploadForm(forms.Form):
-        album = forms.CharField()
-        images = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    album = forms.CharField()
+    images = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+
+class CollectionUpdateForm(forms.ModelForm):
+    images = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+    class Meta:
+        model = Collection
+        fields = (
+            'title',
+            'images'
+        )
+
+
+class PictureAdminForm(forms.ModelForm):
+    images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}),
+                             label="Ladda upp flera bilder",
+                             required=False)
+
+    class Meta:
+        model = Collection
+        fields = '__all__'
+
+    def save(self, *args, **kwargs):
+        collection = super(PictureAdminForm, self).save(*args, **kwargs)
+        if hasattr(self.files, 'getlist'):
+            print(self.files.getlist('images'))
+            for f in self.files.getlist('images'):
+                Picture.objects.create(collection=collection, image=f)
+        return collection
