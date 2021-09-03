0799afdb1999d234ee044abe143b840a5d009484
andreedd <ddeeh2@gmail.com>
2021-09-03 20:29:56 +0300

    members urls.py: changed acitvate from url to path

--- members/urls.py
+++ members/urls.py
@@ -7,8 +7,7 @@ app_name = 'members'
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     url('^', include('django.contrib.auth.urls')),
     path('info/', views.EditView.as_view(), name='info'),
 ]
