45a78dc1783664ca74ac16cb85064539a346c6bf
André Nordström <ddeeh2@gmail.com>
2021-10-12 17:27:53 +0300

    sort extra event fields by id

--- events/models.py
+++ events/models.py
@@ -106,7 +106,7 @@ class Event(models.Model):
     def get_registration_form(self):
         if EventRegistrationForm.objects.filter(event=self).count() == 0:
             return None
-        return EventRegistrationForm.objects.filter(event=self)
+        return EventRegistrationForm.objects.filter(event=self).order_by('-id')
 
     def get_registration_form_public_info(self):
         return EventRegistrationForm.objects.filter(event=self, public_info=True)
