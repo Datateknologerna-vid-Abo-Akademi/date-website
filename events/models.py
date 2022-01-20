36018373f6d9bd76d3281f863160554f3fc9b5f2
WilliamNylund <william.nylund@gmail.com>
2022-01-20 22:53:18 +0200

    Add validation for unique emails on event registration form

--- events/models.py
+++ events/models.py
@@ -13,6 +13,7 @@ from django.db.models import Max
 from django.template.defaulttags import register
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 
 logger = logging.getLogger('date')
 
@@ -114,7 +115,7 @@ class Event(models.Model):
     def make_registration_form(self, data=None):
         if self.sign_up:
             fields = {'user': forms.CharField(label='Namn', max_length=255),
-                      'email': forms.EmailField(label='Email'),
+                      'email': forms.EmailField(label='Email', validators=[self.validate_unique_email]),
                       'anonymous': forms.BooleanField(label='Anonymt', required=False)}
             if self.get_registration_form():
                 for question in reversed(self.get_registration_form()):
@@ -133,6 +134,11 @@ class Event(models.Model):
     def show_attendee_list(self):
         return self.event_date_end > now() + timedelta(-1)
 
+    def validate_unique_email(self, email):
+        attendees = self.get_registrations()
+        for attendee in attendees:
+            if email == attendee.email:
+                raise ValidationError(_("Det finns redan någon anmäld med denna email"))
 
 class EventRegistrationForm(models.Model):
     event = models.ForeignKey(Event, verbose_name='Event', on_delete=models.CASCADE)
