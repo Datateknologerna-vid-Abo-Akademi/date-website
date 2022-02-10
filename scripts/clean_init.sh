79e402d6d9de4a62ab92e778e4a5c589828c7c0c
dny <danneenordstrom@gmail.com>
2022-02-10 21:02:37 +0200

    Clean up stuff and add some comments

--- scripts/clean_init.sh
+++ scripts/clean_init.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 echo "WARNING! This will restore your project to an intial state"
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? y/n " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -11,16 +11,21 @@ fi
 
 source ../example.env
 COMPOSE_PATH="../docker-compose.yml"
-DB_NAME=date-website-db-1
+DB_NAME=date-website-db-1 # Name of your database container
 
+# Shut down any currently running containers
 docker-compose down
 
+# Delete all existing migration files
 find ../ -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+# Start the database container
 docker-compose -f $COMPOSE_PATH build
 docker-compose -f $COMPOSE_PATH up -d db
 
 sleep 2
 
+# Connect to a temporary database to delete and recreate the postgres database
 docker exec $DB_NAME psql -U postgres -c "CREATE DATABASE temp;"
 docker exec $DB_NAME psql -U postgres -d temp -c "DROP DATABASE postgres;"
 docker exec $DB_NAME psql -U postgres -d temp -c "CREATE DATABASE postgres;"
@@ -29,6 +34,7 @@ docker exec $DB_NAME psql -U postgres -c "DROP DATABASE temp;"
 echo "Database cleared."
 echo "Deleting migration files"
 
+# Run migrations on fresh database and load fixture data
 docker-compose -f $COMPOSE_PATH run web python /code/manage.py makemigrations
 docker-compose -f $COMPOSE_PATH run web python /code/manage.py migrate
 docker-compose -f $COMPOSE_PATH run web python /code/manage.py loaddata initialdata.json
@@ -36,6 +42,7 @@ docker-compose down
 
 sleep 2
 
-docker-compose up -d
+# (optional) start website
+# docker-compose up -d
 
 echo "All migrations and data cleared, initial website data loaded."
