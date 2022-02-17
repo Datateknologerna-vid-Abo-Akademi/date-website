a3f6b0940cb522fa88d8882450a523aa46fae12b
André Nordström <33100278+andreedd@users.noreply.github.com>
2022-02-17 18:36:06 +0200

    events avec & news pagination (#226)
    * sort extra event fields by id
    
    * able to sign up on unpublished events
    
    * polls: superuser is able to delete voter in case of complications
    
    * Fix old event filtering
    
    * Fix sär skrivning
    
    * bootstrap: navbar redesigned
    
    * bootstrap: news updated to use bootstrap
    
    * bootstrap: footer using bootstrap
    
    * bootstrap: footer code format
    
    * bootstrap: events index
    
    * bootstrap: events detail done
    
    * bootstrap: staticpages done
    
    * bootstrap: polls done
    
    * bootstrap: members info done
    
    * bootstrap: members bootstrap adjusted
    
    * bootstrap: archive documents done
    
    * bootstrap: archive picture years done
    
    * bootstrap: picture index years better styling
    
    * bootstrap: picture gallery done
    
    * bootstrap: archive upload done
    
    * bootstrap: fontpage working with bootstrap
    
    * bootstrap: move event card chevron-right on hover
    
    * add radius to pagination
    
    * bootstrap: frontpage container-fluid
    
    * bootstrap: update stylesheet links
    
    * bootstrap: add overflow scroll to news
    
    * bootstrap: frontpage working with bootstrap
    
    * font-awesome: base template head
    
    * bootstrap: changed container padding
    
    * bootstrap: center no active events text
    
    * add empty
    
    * Add validation for unique emails on event registration form
    
    * css: change text color into spearate variable
    
    * Add initial data fixture file
    
    * Add firststart 'date-init' command
    
    * Add script to reset website
    
    * Move line
    
    * Find issues
    
    * Changies
    
    * Clean up stuff and add some comments
    
    * Use compose to not hardcode stuff like dumdum
    
    * Add comment to readme
    
    * More readme changes
    
    * Change deprecated import
    
    * arsfest: working arsfest page with avec
    
    * arsfest: form label font size
    
    * arsfest: glitchy albin header
    
    * events form: more mobile friendly form
    
    * events form: more mobile friendly form
    
    * arsfest: font size normal for form
    
    * arsfest: sick colors & animation
    
    * arsfest: css blur fallback if no browser support
    
    * arsfest: fallback opacity lower
    
    * add pagination
    
    * arsfest: background more opacity
    
    * news pagination: added latest css & also added to aa_news
    
    * Add avec functionality to events
    
    * websocket for avec
    
    * Filter avec_for dropdown on event admin page
    
    * Add avec_for column to export attendee list
    
    * Fix preferences admin form field on events
    
    Co-authored-by: dny <danneenordstrom@gmail.com>
    Co-authored-by: WilliamNylund <william.nylund@gmail.com>
    Co-authored-by: hmartola <64286599+hmartola@users.noreply.github.com>

---
+++ scripts/clean_init.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -Eeuo pipefail
+
+echo "WARNING! This will restore your project to an intial state"
+read -p "Are you sure? y/n " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+source ../example.env
+COMPOSE_PATH="../docker-compose.yml"
+
+# Shut down any currently running containers
+docker-compose down
+
+# Delete all existing migration files
+find ../ -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+# Start the database container
+docker-compose -f $COMPOSE_PATH build
+docker-compose -f $COMPOSE_PATH up -d db
+
+sleep 2
+
+# Connect to a temporary database to delete and recreate the postgres database
+docker-compose exec db psql -U postgres -c "CREATE DATABASE temp;"
+docker-compose exec db psql -U postgres -d temp -c "DROP DATABASE postgres;"
+docker-compose exec db psql -U postgres -d temp -c "CREATE DATABASE postgres;"
+docker-compose exec db psql -U postgres -c "DROP DATABASE temp;"
+
+echo "Database cleared."
+echo "Deleting migration files"
+
+# Run migrations on fresh database and load fixture data
+docker-compose -f $COMPOSE_PATH run web python /code/manage.py makemigrations
+docker-compose -f $COMPOSE_PATH run web python /code/manage.py migrate
+docker-compose -f $COMPOSE_PATH run web python /code/manage.py loaddata initialdata.json
+docker-compose down
+
+sleep 2
+
+# (optional) start website
+# docker-compose up -d
+
+echo "All migrations and data cleared, initial website data loaded."
