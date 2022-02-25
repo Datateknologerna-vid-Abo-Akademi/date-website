52b69746a43d8917f151bae2f6ebf36411243553
André Nordström <ddeeh2@gmail.com>
2022-02-25 16:10:47 +0200

    change jsonfield import

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ import logging
 
 from ckeditor.fields import RichTextField
 from django import forms
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models import Max
