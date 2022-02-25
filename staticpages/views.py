1aecaeb864bbc9bbeb2cf569348b3871f3663f26
André Nordström <ddeeh2@gmail.com>
2022-02-25 17:30:12 +0200

    staticpages: option to enable staticpage content only when logged in

--- staticpages/views.py
+++ staticpages/views.py
@@ -13,7 +13,8 @@ class StaticPageIndex(View):
 class StaticPageView(View):
     def get(self, request, slug):
         page = models.StaticPage.objects.get(slug=slug)
-        return render(request, 'staticpages/staticpage.html', {'page':page})
+        show_content = not page.members_only or (page.members_only and request.user.is_authenticated)
+        return render(request, 'staticpages/staticpage.html', {'page':page, 'show_content': show_content})
 
 def staticUrl(request):
     staticUrls = models.StaticUrl.objects.all()
