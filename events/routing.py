304cd6eaa056f24d9209dd8d80cc4da9cd34cc3d
Jesse Aspnäs <jesse@jaspnas.com>
2023-02-08 02:29:02 +0200

    Fix events websocket by installing daphne as asgi server

--- events/routing.py
+++ events/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/events/(?P<event_name>\w+)/$', consumers.EventConsumer),
+    re_path(r'ws/events/(?P<event_name>\w+)/$', consumers.EventConsumer.as_asgi()),
 ]
