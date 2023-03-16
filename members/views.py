e3ed36007c530879cac5599fcb661880cd117910
Jesse Aspnäs <jesse@jaspnas.com>
2023-03-16 19:45:26 +0200

    Add admin password reset and reformat members module (#290)
    * Add admin password reset and reformat members module
    
    * Apply migrations

--- members/views.py
+++ members/views.py
@@ -20,11 +20,13 @@ from .tokens import account_activation_token
 
 logger = logging.getLogger('date')
 
+
 class EditView(View):
 
     def get(self, request):
         user = request.user
         return render(request, 'userinfo.html', {"user": user})
+
 
 class CertificateView(View):
     def get(self, request):
@@ -33,13 +35,13 @@ class CertificateView(View):
         current_time = datetime.datetime.now()
 
         icon_options = {
-            'Monday' : icons[0],
-            'Tuesday' : icons[1],
-            'Wednesday' : icons[2],
-            'Thursday' : icons[3],
-            'Friday' : icons[4],
-            'Saturday' : icons[5],
-            'Sunday' : icons[6],
+            'Monday': icons[0],
+            'Tuesday': icons[1],
+            'Wednesday': icons[2],
+            'Thursday': icons[3],
+            'Friday': icons[4],
+            'Saturday': icons[5],
+            'Sunday': icons[6],
         }
         icon = icon_options[current_time.strftime("%A")]
 
@@ -72,7 +74,7 @@ def signup(request):
             })
             to_email = os.environ.get('EMAIL_HOST_RECEIVER')
             email = EmailMessage(
-                        mail_subject, message, to=[to_email]
+                mail_subject, message, to=[to_email]
             )
             logger.info(f"NEW USER: Sending email to {to_email}")
             email.send()
