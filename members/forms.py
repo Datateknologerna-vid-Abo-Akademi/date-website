f7c5ee1a7cefb0882517e96acc9bf7bbffdeedd1
dny <danneenordstrom@gmail.com>
2023-05-08 20:49:51 +0300

    Create form for alumnistuff

--- members/forms.py
+++ members/forms.py
@@ -142,6 +142,24 @@ class SignUpForm(forms.ModelForm):
         )
 
 
+class AlumniSignUpForm(forms.Form):
+    tfif_choices = (
+        ('ja', 'Ja'),
+        ('nej', 'Nej'),
+        ('vet inte', 'Vet inte')
+    )
+
+    name = forms.CharField(max_length=200, required=True, help_text=_('detta fält är obligatoriskt'), label=_('Namn'))
+    email = forms.EmailField(max_length=200, help_text=_('detta fält är obligatoriskt'), label=_('E-postadress'), required=True)
+    phone_number = forms.CharField(max_length=20, label=_('Telefonnummer'), required=False)
+    address = forms.CharField(max_length=100, label=_('Adress'), required=False)
+    year_of_admission = forms.IntegerField(max_value=3000, label=_('Inskrivningsår'), required=False)
+    employer = forms.CharField(max_length=200, label=_('Arbetsplats'), required=False)
+    work_title = forms.CharField(max_length=200, label=_('Arbetsuppgift'), required=False)
+    tfif_membership = forms.ChoiceField(choices=tfif_choices, label=_('TFiF medlemskap'), required=False)
+    alumni_newsletter_consent = forms.BooleanField(label=_('Jag tar gärna emot information om alumnevenemang'), required=False)
+
+
 class SubscriptionPaymentChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         if not obj.first_name or not obj.last_name:
