aa87dca30cccc8cd860603e252d310b75193f52d
dny <danneenordstrom@gmail.com>
2023-05-08 20:49:16 +0300

    Add view for alumni signups and emails

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 import datetime
 import logging
 import os
+import datetime
+from smtplib import SMTPException
 
 from django.contrib.auth.hashers import make_password
 from django.contrib.sites.shortcuts import get_current_site
@@ -13,8 +15,9 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 
-from members.forms import SignUpForm
-from .models import Member
+from members.forms import SignUpForm, AlumniSignUpForm
+
+from .models import Member, AlumniEmailRecipient
 from .tokens import account_activation_token
 
 logger = logging.getLogger('date')
@@ -96,3 +99,33 @@ def activate(request, uidb64, token):
         return render(request, 'userinfo.html', {"user": user, "msg": msg})
     else:
         return HttpResponse('Activation link is invalid!')
+
+
+def alumni_signup(request):
+    '''Handle signup form and email sending for new alumnis'''
+
+    form = AlumniSignUpForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        # Mail to the person signing up
+        alumni_email = form.cleaned_data['email']
+        alumni_message_subject = "Välkommen till ARG - Betalningsinstruktioner"
+        alumni_message_content = render_to_string('alumni_signup_email.html')
+        final_alumni_email = EmailMessage(alumni_message_subject, alumni_message_content, to=[alumni_email])
+
+        # Mail to relevant people
+        admin_message_recipients = list(AlumniEmailRecipient.objects.all().values_list('recipient_email', flat=True))
+        admin_message_subject = f"ARG - Ny medlem {form.cleaned_data['name']}"
+        admin_message_content = render_to_string('alumni_signup_email_admin.html', {'alumni': form.cleaned_data})
+        final_admin_email = EmailMessage(admin_message_subject, admin_message_content, to=admin_message_recipients)
+
+        # Send mails
+        try:
+            final_alumni_email.send()
+            final_admin_email.send()
+        except SMTPException:
+            logger.error("Failed to send alumni signup email: %s", form.cleaned_data['name'])
+
+        return render(request, 'registration/registration_complete.html', {'alumni': True})
+
+    return render(request, 'signup.html', {'form': form, 'alumni': True})
