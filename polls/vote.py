8b6a6e49bfbdb612bc37efa35c3d63bf6db0253c
André Nordström <33100278+andreedd@users.noreply.github.com>
2023-12-08 19:21:50 +0200

    fix error generated from non logged in users (#425)

--- polls/vote.py
+++ polls/vote.py
@@ -38,7 +38,10 @@ def is_user_authorized_to_vote(question, user):
     if question.voting_options == ANYONE:
         return True
 
-    if question.voting_options == MEMBERS_ONLY and user.is_authenticated:
+    if not user.is_authenticated:
+        return False
+
+    if question.voting_options == MEMBERS_ONLY:
         return True
 
     if question.voting_options == ORDINARY_MEMBERS_ONLY and user.membership_type == ORDINARY_MEMBER:
