fa30aad7bbd35930a7a4ac80a7bcb2c04dabd510
André Nordström <33100278+andreedd@users.noreply.github.com>
2024-01-03 16:46:14 +0200

    Jubbe countdown (#437)
    * Build image for both amd64 and arm64 (#432)
    
    * Jubbe 25 (#436)
    
    * 25 countdown page
    
    ---------
    
    Co-authored-by: Jesse Aspnäs <jesse@jaspnas.com>

--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', date.index, name='index'),
+    path('25', date.date_25, name='date_25'),
     path('news/', include('news.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
