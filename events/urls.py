a38bcc98ca9a9e85ad740879833f779f27c575e1
Jesse Aspnäs <jesse@jaspnas.com>
2024-01-16 22:57:59 +0200

    Add caching to rarely updated pages (#450)
    * Add caching to rarely updated pages
    
    5 min TTL for main page
    
    1 minute TTL for index- and staticpages
    
    * Make web container depend on redis
    
    Tests will otherwise fail because of caching

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,12 @@
 from django.urls import re_path
+from django.views.decorators.cache import cache_page
+
 from . import feed, views
 
 app_name = 'events'
 
 urlpatterns = [
-    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^$', cache_page(60)(views.IndexView.as_view()), name='index'),
     re_path(r'^(?P<slug>[-\w]+)/$', views.EventDetailView.as_view(), name='detail'),
     re_path(r'^feed$', feed.EventFeed(), name='feed'),
 ]
