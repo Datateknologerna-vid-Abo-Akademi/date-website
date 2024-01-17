b23add3572a99f23dc3599f17dfc4102bfec949c
Jesse Aspnäs <jesse@jaspnas.com>
2024-01-17 15:01:07 +0200

    Move caching to templates (#454)

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, re_path
-from django.views.decorators.cache import cache_page
+from django.urls import re_path
 
 from . import views
 
 app_name = 'staticpages'
 
 urlpatterns = [
-    re_path(r'^(?P<slug>[-\w]+)/$', cache_page(60)(views.StaticPageView.as_view()), name='page'),
+    re_path(r'^(?P<slug>[-\w]+)/$', views.StaticPageView.as_view(), name='page'),
 ]
