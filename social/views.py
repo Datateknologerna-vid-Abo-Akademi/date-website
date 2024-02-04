85983ff603fc9ee4b2f347e90e6cf9af230952c9
keanderss <kenzu.2000172@gmail.com>
2024-02-04 06:26:41 +0200

    Fixed automatic form resubmission when refreshing social harassment success page

--- social/views.py
+++ social/views.py
@@ -10,13 +10,18 @@ def socialIndex(request):
 
 def harassment_form(request):
     form = HarassmentForm()
+    # If user has submitted the form show success page
+    if request.session.get("has_submitted", False):
+        return render(request, 'social/harassment_success.html')
 
     if request.method == 'POST':
         form = HarassmentForm(request.POST)
         if form.is_valid():
             form.save()  # Save the form data to the Harassment model
+            request.session['has_submitted'] = True
+            request.session.set_expiry(10)
             # Redirect to a success page or perform other actions
-            return render(request, 'social/harassment_success.html', {'form': form})  # Redirect to a success page
+            return redirect(request.path)  # Redirect to a success page
 
     # If the form is not valid or it's a GET request, re-render the form with errors
     return render(request, 'social/harassment_form.html', {'form': form})
