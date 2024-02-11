6bbdf07f88ed317a554a8e0a586d3c8f6aab619f
Theoabw <theo@wilenius.eu>
2024-02-12 01:19:31 +0200

    made email non-editable, name & last name required

--- members/forms.py
+++ members/forms.py
@@ -250,8 +250,9 @@ class FunctionaryFilterForm(forms.Form):
 
 
 class MemberEditForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = Member
-        fields = ['email', 'first_name', 'last_name', 'phone', 'address', 'zip_code', 'city', 'country']
+        fields = ['first_name', 'last_name', 'phone', 'address', 'zip_code', 'city', 'country']
