947221b2bbaec1a3ec3f54b8383ed78ea8a925f4
Theo Wilenius <theo@wilenius.eu>
2024-03-21 23:32:31 +0200

    update to ckeditor 5 (#519)
    * update to ckeditor5
    
    * ckeditor5 config
    
    * remove old config
    
    * update ckeditor syles, use extends config
    
    * made extends config default

--- staticpages/models.py
+++ staticpages/models.py
@@ -1,6 +1,6 @@
 import logging
 
-from ckeditor.fields import RichTextField
+from django_ckeditor_5.fields import CKEditor5Field
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -20,7 +20,7 @@ class StaticPageNav(models.Model):
 
 class StaticPage(models.Model):
     title = models.CharField(_('Titel'), max_length=255, blank=False)
-    content = RichTextField(_('Innehåll'), blank=True)
+    content = CKEditor5Field(_('Innehåll'), blank=True)
     created_time = models.DateTimeField(_('Skapad'), default=timezone.now)
     modified_time = models.DateTimeField(_('Modifierad'), editable=False, null=True, blank=True)
     slug = models.SlugField(_('Slug'), unique=True, allow_unicode=False, max_length=POST_SLUG_MAX_LENGTH)
