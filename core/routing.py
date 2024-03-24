f01fc640d27092604acef5096936d54618846196
Jesse Aspnäs <jesse@jaspnas.com>
2024-03-25 00:15:29 +0200

    Use separate containers for wsgi and asgi with reverse proxy in production

--- core/routing.py
+++ core/routing.py
@@ -2,7 +2,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+import os
 import events.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
