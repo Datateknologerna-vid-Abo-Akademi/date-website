cbf03b87e36ef76dcc5e6e0de97794ac998a1081
Jesse Aspnäs <jesse@jaspnas.com>
2024-03-31 07:06:57 +0300

    Fix more template paths

--- date/views.py
+++ date/views.py
@@ -74,12 +74,12 @@ def language(request, lang):
 
 
 def handler404(request, *args, **argv):
-    response = render(request, '404.html', {})
+    response = render(request, 'core/404.html', {})
     response.status_code = 404
     return response
 
 
 def handler500(request, *args, **argv):
-    response = render(request, '500.html', {})
+    response = render(request, 'core/500.html', {})
     response.status_code = 404
     return response
