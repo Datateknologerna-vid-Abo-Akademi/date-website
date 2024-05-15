9074d878d3e2af7af41a6fa1bc70930105975c64
Jesse Aspnäs <jesse@jaspnas.com>
2024-05-15 22:21:57 +0300

    Fix author url returning 404 (#614)

--- news/urls.py
+++ news/urls.py
@@ -6,11 +6,11 @@ app_name = 'news'
 
 urlpatterns = [
     path('feed/', feed.LatestPosts()),
+    # This is a mess because of lack of username validation
+    re_path(r'author/(?P<author>[\w\s.@\u00C0-\u00FF\u00C5\u00C4\u00D6\u00E5\u00E4\u00F6-]+)/$', views.author,
+            name='author'),
     path('', views.index, name='index'),
     path('<slug:category>/', views.category_index, name='aa_index'),
     path('articles/<slug:slug>/', views.article, name='detail'),
     path('<slug:category>/<slug:slug>/', views.category_article, name='detail'),
-    # This is a mess because of lack of username validation
-    re_path(r'author/(?P<author>[\w\s.@\u00C0-\u00FF\u00C5\u00C4\u00D6\u00E5\u00E4\u00F6-]+)/$', views.author,
-            name='author'),
 ]
