d9e8022e9ff86e1dd24adefca2e8dc17d83a3ab1
Jesse Aspnäs <jesse@jaspnas.com>
2024-12-16 20:56:41 +0200

    Barely working functionality for ÖN

--- events/routing.py
+++ events/routing.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import re_path
 
 from . import consumers
@@ -5,3 +6,9 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/events/(?P<event_name>\w+)/$', consumers.EventConsumer.as_asgi()),
 ]
+
+# TODO: Fix this in a better way
+if settings.PROJECT_NAME == 'on':
+    websocket_urlpatterns = [
+        re_path(r'ws/(?P<event_name>\w+)/$', consumers.EventConsumer.as_asgi()),
+    ]
