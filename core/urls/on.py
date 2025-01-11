45a4eb0297708141a3194281b4f8b98e4950462e
Jesse Aspnäs <jesse@jaspnas.com>
2025-01-11 15:19:29 +0200

    Crimes against design

--- core/urls/on.py
+++ core/urls/on.py
@@ -27,9 +27,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', events.IndexView.as_view(), name='index'),
+    path('admin/', wadmin.site.urls),
     path('', include('events.urls')),
     path('pages/', include('staticpages.urls')),
-    path('admin/', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
