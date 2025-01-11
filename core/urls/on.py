ee1d2a7753f461c9b27da59f5432a937468e4335
Jesse Aspnäs <jesse@jaspnas.com>
2025-01-11 16:29:58 +0200

    Add ön config (#685)
    * Barely working functionality for ÖN
    
    * Crimes against design

---
+++ core/urls/on.py
@@ -0,0 +1,40 @@
+"""date URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# update
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from events import views as events
+from date import views as date
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', events.IndexView.as_view(), name='index'),
+    path('admin/', wadmin.site.urls),
+    path('', include('events.urls')),
+    path('pages/', include('staticpages.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = date.handler404
+handler500 = date.handler500
