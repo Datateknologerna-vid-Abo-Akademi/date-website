41bbbc77ced98c4019ec6eaae6053f898d69554a
Jesse Aspnäs <jesse@jaspnas.com>
2025-04-22 18:42:26 +0300

    Make field appear in signup form

--- members/forms.py
+++ members/forms.py
@@ -19,7 +19,7 @@ logger = logging.getLogger('date')
 
 class MemberCreationForm(forms.ModelForm):
     send_email = forms.BooleanField(required=False)
-    year_of_admission = forms.IntegerField(initial=datetime.datetime.now().year, required=False)
+    year_of_admission = forms.IntegerField(initial=datetime.datetime.now().year, required=False, label=_('Inskrivningsår'))
 
     password = forms.CharField(
         widget=forms.PasswordInput(),
@@ -159,6 +159,7 @@ class SignUpForm(forms.ModelForm):
     )
     first_name = forms.CharField(max_length=100, required=True, label=_('Förnamn'))
     last_name = forms.CharField(max_length=100, required=True, label=_('Efternamn'))
+    year_of_admission = forms.IntegerField(initial=datetime.datetime.now().year, required=False, label=_('Inskrivningsår'))
 
     class Meta:
         model = Member
@@ -173,6 +174,7 @@ class SignUpForm(forms.ModelForm):
             'city',
             'country',
             'membership_type',
+            'year_of_admission',
             'password'
         )
 
