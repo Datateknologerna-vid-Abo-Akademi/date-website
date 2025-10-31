89eed294fd4bf389542abd6176b6104846aea9b0
Theo Wilenius <theo@wilenius.eu>
2025-10-31 19:21:27 +0200

    member id fix attempt

--- members/migrations/0004_functionaryrole_membershiptype_and_more.py
+++ members/migrations/0004_functionaryrole_membershiptype_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0003_auto_20200109_1909'),
+        ('members', '0004'),
     ]
 
     operations = [
