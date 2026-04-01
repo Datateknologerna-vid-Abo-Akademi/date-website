1b3c6ec82219023ab99141f9448f7f725c3c635f
Theo Wilenius <theo@wilenius.eu>
2026-04-02 00:34:06 +0300

    Update Pulterit site branding and language toggle

--- core/urls/pulterit.py
+++ core/urls/pulterit.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('publications/', include('publications.urls')),
+    path('set_lang/', date.set_language, name="set_lang"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
