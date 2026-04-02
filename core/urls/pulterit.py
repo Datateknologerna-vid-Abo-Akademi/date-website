26b52cc5f8df5d501c852260ecdd4d01751fdee6
Theoabw <theo@wilenius.eu>
2026-04-02 23:02:47 +0300

    feat: refresh pulterit site templates and styling

--- core/urls/pulterit.py
+++ core/urls/pulterit.py
@@ -22,10 +22,11 @@ from django.urls import include, path
 
 from events import views as events
 from date import views as date
+from core.urls.common import build_urlpatterns
 
 app_name = 'core'
 
-urlpatterns = [
+urlpatterns = build_urlpatterns(
     path('', date.index, name='index'),
     path('news/', include('news.urls')),
     path('members/', include('members.urls')),
@@ -39,8 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('publications/', include('publications.urls')),
-    path('set_lang/', date.set_language, name="set_lang"),
-]
+)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
