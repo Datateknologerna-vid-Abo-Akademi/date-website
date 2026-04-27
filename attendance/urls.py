9bf23aebf77225345ee8cf1c15075c776e54dc0e
Mixu78 <kantola.miksu@gmail.com>
2026-04-27 22:46:04 +0300

    Use unique name for attendance index page

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AttendanceEventsView.as_view(), name="index"),
+    path("", views.AttendanceEventsView.as_view(), name="attendance-index"),
     path("<slug:slug>/", views.AttendanceEventDetailView.as_view(), name="attendance-event-view"),
     path("<slug:slug>/overview", views.AttendanceEventOverview.as_view(), name="attendance-event-overview")
 ]
