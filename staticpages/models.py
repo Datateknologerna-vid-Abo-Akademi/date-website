819377e7e94c9171749a77edae89f207e9c976cc
Theo Wilenius <theo@wilenius.eu>
2026-05-02 22:21:06 +0300

    add functionality to add images to static pages (#950)
    * add functionality to add images to static pages
    
    * Add static page image migration
    
    * Address static page background review comments
    
    ---------
    
    Co-authored-by: Akseli Forma <akseliforma@gmail.com>

--- staticpages/models.py
+++ staticpages/models.py
@@ -1,15 +1,29 @@
 import logging
+import os
 
 from django_ckeditor_5.fields import CKEditor5Field
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+
+from core.fields import PublicFileField
 
 logger = logging.getLogger('date')
 
 POST_SLUG_MAX_LENGTH = 50
 
+def upload_to(instance, filename):
+    filename_base, filename_ext = os.path.splitext(filename)
+
+    file_location = "pages/{filename}{extension}".format(
+        filename=slugify(filename_base),
+        extension=filename_ext.lower(),
+    )
+    return file_location
 
 class StaticPageNav(models.Model):
     category_name = models.CharField(_('Kategori'), max_length=255, blank=False)
@@ -28,12 +42,41 @@ class StaticPage(models.Model):
     modified_time = models.DateTimeField(_('Modifierad'), editable=False, null=True, blank=True)
     slug = models.SlugField(_('Slug'), unique=True, allow_unicode=False, max_length=POST_SLUG_MAX_LENGTH)
     members_only = models.BooleanField(_('Kräv inloggning'), default=False)
+    image = models.ImageField(_('Bakgrundsbild'), null=True, blank=True, upload_to=upload_to)
+    s3_image = PublicFileField(verbose_name=_('Bakgrundsbild'), null=True, blank=True, upload_to=upload_to)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('staticpages:page', args=[self.slug])
+
+    def clean(self):
+        super().clean()
+        if self.image and self.s3_image:
+            raise ValidationError({
+                'image': _('Välj antingen en lokal bakgrundsbild eller en publik S3-bakgrundsbild, inte båda.'),
+                's3_image': _('Välj antingen en lokal bakgrundsbild eller en publik S3-bakgrundsbild, inte båda.'),
+            })
+
+    @property
+    def background_image_url(self):
+        field_names = ("s3_image", "image") if settings.USE_S3 else ("image", "s3_image")
+        for field_name in field_names:
+            field = getattr(self, field_name, None)
+            if not field:
+                continue
+            try:
+                return field.url
+            except Exception as exc:
+                logger.warning(
+                    "Unable to resolve %s URL for static page %s (%s): %s",
+                    field_name,
+                    self.pk,
+                    self.slug or self.title,
+                    exc,
+                )
+        return ""
 
     def update(self):
         self.modified_time = timezone.now()
