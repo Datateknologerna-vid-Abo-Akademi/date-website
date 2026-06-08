f0d806f046ed2fabd91063070dc0ed472aad55d4
Jesse Aspnäs <jesse@jaspnas.com>
2026-06-08 21:10:33 +0300

    Register AlumniEmailRecipient in admin

--- alumni/admin.py
+++ alumni/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from alumni.models import AlumniEmailRecipient
+
+
+@admin.register(AlumniEmailRecipient)
+class AlumniEmailRecipientAdmin(admin.ModelAdmin):
+    list_display = ("recipient_email",)
+    search_fields = ("recipient_email",)
