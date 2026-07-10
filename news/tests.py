b519be29e0a6e517890a1751f9f2efd38c51fe55
Theoabw <theo@wilenius.eu>
2026-07-11 02:17:09 +0300

    Use generic page_obj in pagination partial, add _page_window tests

--- news/tests.py
+++ news/tests.py
@@ -42,3 +42,119 @@ class NewsTestCase(TestCase):
         c = Client()
         response = c.get(reverse('news:detail', args=[self.post.slug]))
         self.assertEqual(response.status_code, 200)
+
+
+class PageWindowTestCase(TestCase):
+    def test_page_window(self):
+        from news.views import _page_window
+
+        cases = [
+            # (current, total, window, expected dict)
+            (
+                1,
+                1,
+                2,
+                {
+                    'page_range_window': range(1, 2),
+                    'show_first_page': False,
+                    'show_last_page': False,
+                    'show_first_ellipsis': False,
+                    'show_last_ellipsis': False,
+                },
+            ),
+            (
+                1,
+                5,
+                2,
+                {
+                    'page_range_window': range(1, 4),
+                    'show_first_page': False,
+                    'show_last_page': True,
+                    'show_first_ellipsis': False,
+                    'show_last_ellipsis': True,
+                },
+            ),
+            (
+                2,
+                5,
+                2,
+                {
+                    'page_range_window': range(1, 5),
+                    'show_first_page': False,
+                    'show_last_page': True,
+                    'show_first_ellipsis': False,
+                    'show_last_ellipsis': False,
+                },
+            ),
+            (
+                3,
+                5,
+                2,
+                {
+                    'page_range_window': range(1, 6),
+                    'show_first_page': False,
+                    'show_last_page': False,
+                    'show_first_ellipsis': False,
+                    'show_last_ellipsis': False,
+                },
+            ),
+            (
+                4,
+                5,
+                2,
+                {
+                    'page_range_window': range(2, 6),
+                    'show_first_page': True,
+                    'show_last_page': False,
+                    'show_first_ellipsis': False,
+                    'show_last_ellipsis': False,
+                },
+            ),
+            (
+                5,
+                5,
+                2,
+                {
+                    'page_range_window': range(3, 6),
+                    'show_first_page': True,
+                    'show_last_page': False,
+                    'show_first_ellipsis': True,
+                    'show_last_ellipsis': False,
+                },
+            ),
+            (
+                5,
+                20,
+                2,
+                {
+                    'page_range_window': range(3, 8),
+                    'show_first_page': True,
+                    'show_last_page': True,
+                    'show_first_ellipsis': True,
+                    'show_last_ellipsis': True,
+                },
+            ),
+            (
+                3,
+                5,
+                0,
+                {
+                    'page_range_window': range(3, 4),
+                    'show_first_page': True,
+                    'show_last_page': True,
+                    'show_first_ellipsis': True,
+                    'show_last_ellipsis': True,
+                },
+            ),
+        ]
+
+        for current, total, window, expected in cases:
+            with self.subTest(current=current, total=total, window=window):
+                page_obj = _fake_page(current, total)
+                result = _page_window(page_obj, window=window)
+                self.assertEqual(result, expected)
+
+
+def _fake_page(number, total):
+    paginator = type('FakePaginator', (), {'num_pages': total})()
+    return type('FakePage', (), {'number': number, 'paginator': paginator})()
