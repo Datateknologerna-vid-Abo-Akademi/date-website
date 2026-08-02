35274aa317d97abb45cd58eb32eeb0951b9663ff
MaxTheMan-1216 <max.ruotsalainen03@gmail.com>
2026-08-02 20:20:24 +0300

    Quickly fixed formatter bug

--- functionaries/selectors.py
+++ functionaries/selectors.py
@@ -53,7 +53,9 @@ def get_selected_role(request, functionary_roles):
         else:
             try:
                 role_id = int(role_param)
-            except (ValueError, TypeError):
+            except ValueError:
+                pass
+            except TypeError:
                 pass
             else:
                 selected_role = FunctionaryRole.objects.filter(pk=role_id).first()
